4de1ccef3afc1fd5f3bbc4f26e297944a5454ea0
melod <austinfairgray@gmail.com>
2025-07-24 21:51:40 +1000

    fixed token generation no longer working

--- SpotifyExplode/Http/SpotifyHttp.cs
+++ SpotifyExplode/Http/SpotifyHttp.cs
@@ -1,14 +1,17 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text.Json.Nodes;
 using System.Threading;
 using System.Threading.Tasks;
+using OtpNet;
 using SpotifyExplode.Utils.Extensions;
 
 namespace SpotifyExplode;
 
 internal class SpotifyHttp(HttpClient http)
 {
+    private static readonly byte[] TotpGenSecret = HexStringToBytes("373935343432353338313131383835343934393434343533353130373735383535393131373934");
     public async ValueTask<string> GetAsync(
         string url,
         CancellationToken cancellationToken = default)
@@ -23,9 +26,11 @@ internal class SpotifyHttp(HttpClient http)
     private async ValueTask<string> GetAccessTokenAsync(
         CancellationToken cancellationToken = default)
     {
+        string totp = CalculateTotp(TotpGenSecret);
+        
         using var request = new HttpRequestMessage(
             HttpMethod.Get,
-            "https://open.spotify.com/get_access_token?reason=transport&productType=web_player"
+            $"https://open.spotify.com/api/token?reason=init&productType=web-player&totp={totp}&totpServer={totp}&totpVer=18"
         );
 
         var tokenJson = await http.ExecuteAsync(request, cancellationToken);
@@ -33,5 +38,22 @@ internal class SpotifyHttp(HttpClient http)
         var spotifyJsonToken = JsonNode.Parse(tokenJson)!;
 
         return spotifyJsonToken["accessToken"]!.ToString();
+    }
+
+    private static string CalculateTotp(byte[] secret)
+    {
+        string base32Secret = Base32Encoding.ToString(secret);
+        var totp = new Totp(Base32Encoding.ToBytes(base32Secret), step: 30, totpSize: 6);
+        string token = totp.ComputeTotp();
+        return token;
+    }
+    
+    private static byte[] HexStringToBytes(string hex)
+    {
+        int length = hex.Length;
+        byte[] bytes = new byte[length / 2];
+        for (int i = 0; i < length; i += 2)
+            bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+        return bytes;
     }
 }
