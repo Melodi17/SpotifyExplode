2568be6986bd8b0b5a0cba4abbcb796effb181fd
Melodi <62381002+Melodi17@users.noreply.github.com>
2025-09-21 10:03:37 +1000

    fixed constant secret issue

--- SpotifyExplode.Tests/TrackSpecs.cs
+++ SpotifyExplode.Tests/TrackSpecs.cs
@@ -8,15 +8,15 @@ public class TrackSpecs
 {
     [Theory]
     [InlineData("0VjIjW4GlUZAMYd2vXMi3b")]
-    public async Task I_can_get_a_download_url_from_a_track(string trackId)
+    public async Task I_can_get_a_track(string trackId)
     {
         // Arrange
         var spotify = new SpotifyClient();
 
         // Act
-        var results = await spotify.Tracks.GetDownloadUrlAsync(trackId);
+        var results = await spotify.Tracks.GetAsync(trackId);
 
         // Assert
-        results.Should().NotBeNullOrWhiteSpace();
+        results.Should().NotBeNull();
     }
 }
--- SpotifyExplode/Http/SpotifyHttp.cs
+++ SpotifyExplode/Http/SpotifyHttp.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Text.Json.Nodes;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,7 +13,8 @@ namespace SpotifyExplode;
 
 internal class SpotifyHttp(HttpClient http)
 {
-    private static readonly byte[] TotpGenSecret = HexStringToBytes("373935343432353338313131383835343934393434343533353130373735383535393131373934");
+    private static byte[]? TotpGenSecret;
+    private static int? TotpGenVer;
     public async ValueTask<string> GetAsync(
         string url,
         CancellationToken cancellationToken = default)
@@ -23,27 +26,80 @@ internal class SpotifyHttp(HttpClient http)
         return await http.ExecuteAsync(request, cancellationToken);
     }
 
+    private void GetLatestSecrets()
+    {
+        // https://github.com/Thereallo1026/spotify-secrets/blob/main/secrets/secrets.json?raw=true
+        /*
+         * import requests
+           
+           # Fetch secrets
+           response = requests.get("https://github.com/Thereallo1026/spotify-secrets/blob/main/secrets/secretDict.json?raw=true")
+           secrets = response.json()
+           
+           # Get latest version
+           latest_secret = secrets[(v := max(secrets, key=int))]
+           print(f"Version {v}: {latest_secret}")
+         */
+
+        const string secretsUrl =
+            "https://github.com/Thereallo1026/spotify-secrets/blob/main/secrets/secretBytes.json?raw=true";
+        
+        var secretsJson = http.ExecuteAsync(secretsUrl).GetAwaiter().GetResult();
+        var secretsArray = JsonNode.Parse(secretsJson)!.AsArray();
+        var latestSecret = secretsArray
+            .Select(node => new
+            {
+                Version = node!["version"]!.GetValue<int>(),
+                Secret = node["secret"]!.AsArray().Select(n => n!.GetValue<byte>()).ToArray()
+            })
+            .OrderByDescending(s => s.Version)
+            .First();
+        
+        var transformed = latestSecret.Secret
+            .Select((b, i) => (byte)(b ^ ((i % 33) + 9)))
+            .ToArray();
+
+        var joined = transformed
+            .Select(b => b.ToString())
+            .Aggregate((a, b) => a + b);
+        
+        var hexStr = string.Concat(joined.Select(c => ((int)c).ToString("x2")));
+        var hexBytes = HexStringToBytes(hexStr);
+        
+        TotpGenVer = latestSecret.Version;
+        TotpGenSecret = hexBytes;
+    }
+
     private async ValueTask<string> GetAccessTokenAsync(
         CancellationToken cancellationToken = default)
     {
-        string totp = CalculateTotp(TotpGenSecret);
+        if (TotpGenSecret == null || TotpGenVer == null)
+            GetLatestSecrets();
+
+        string totp = CalculateTotp();
+        
+        string product = "app";
+        string reason = "transport";
         
         using var request = new HttpRequestMessage(
             HttpMethod.Get,
-            $"https://open.spotify.com/api/token?reason=transport&productType=app&totp={totp}&totpServer={totp}&totpVer=18"
+            $"https://open.spotify.com/api/token?reason={reason}&productType={product}&totp={totp}&totpServer={totp}&totpVer={TotpGenVer}"
         );
 
         var tokenJson = await http.ExecuteAsync(request, cancellationToken);
-
         var spotifyJsonToken = JsonNode.Parse(tokenJson)!;
 
         return spotifyJsonToken["accessToken"]!.ToString();
     }
-
-    private static string CalculateTotp(byte[] secret)
+    
+    private static string CalculateTotp()
     {
-        string base32Secret = Base32Encoding.ToString(secret);
-        var totp = new Totp(Base32Encoding.ToBytes(base32Secret), step: 30, totpSize: 6);
+        // string base32Secret = Base32Encoding.ToString(secret);
+        // var totp = new Totp(Base32Encoding.ToBytes(base32Secret), step: 30, totpSize: 6);
+        // string token = totp.ComputeTotp();
+        // return token;
+        
+        var totp = new Totp(SpotifyHttp.TotpGenSecret, step: 30, totpSize: 6);
         string token = totp.ComputeTotp();
         return token;
     }
